ee11229f87bc973b1df94b1c59357f9e06268ca6
vg006 <devvg006@gmail.com>
2025-06-16 13:39:29 +0530

    🐛 fix: Fixed schema
    Added:
     + Password constraints
    
    Changed:
     ~ SignUpSchema

--- src/lib/validators/schema.ts
+++ src/lib/validators/schema.ts
@@ -16,22 +16,34 @@ const passwordValidator = v.pipe(
 const passwordRequiredValidator = v.pipe(
   v.string(),
   v.nonEmpty('Password is required'),
+  v.minLength(8, 'Password must be at least 8 characters'),
+  v.maxLength(12, 'Password must be less than 12 characters'),
 );
 
 const nameValidator = v.pipe(
   v.string(),
-  v.nonEmpty('Name is required'),
+  v.nonEmpty('Name shouldn\'t be empty'),
   v.maxLength(20, 'Name must be less than 20 characters'),
 );
 
 export const SignUpSchema = v.object({
-  name: nameValidator,
+  full_name: v.pipe(
+    nameValidator,
+    v.regex(/^[A-Za-z ]+$/, "Full name must contain only alphanumeric characters"),
+  ),
+  username: v.pipe(
+    nameValidator,
+    v.regex(/^\w+$/, "Username must contain only alphanumeric characters"),
+  ),
   email: emailValidator,
   password: passwordValidator,
 });
 
 export const SignInSchema = v.object({
-  email: emailValidator,
+  username: v.pipe(
+    nameValidator,
+    v.regex(/^\w+$/, "Username must contain only alphanumeric characters"),
+  ),
   password: v.pipe(
     passwordRequiredValidator,
     passwordValidator
